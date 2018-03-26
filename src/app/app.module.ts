92d9dbbaa073ead7b6c6dceef22700007bdf1ccb
ceoehis <ceo.ehis@outlook.com>
2018-03-26 03:45:17 +0100

    add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
