591de68c832893ac81237118deea29d48cb37691
ceoehis <ceo.ehis@outlook.com>
2018-03-26 04:45:10 +0100

    display subcategories from sidebar

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../itemsdata.json';
+interface Subcategory {
+  name: string,
+  items: Object[]
+}
 
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.scss']
 })
-export class ShoppingComponent implements OnInit {
 
+
+export class ShoppingComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+  
+  public itemsList = data;
 
+  public subcategory: Subcategory = data[0].subcategories[0];
+
+  display(subcategory: Subcategory) {
+    console.log(subcategory);
+    this.subcategory = subcategory;
+    console.log('sub is: ', this.subcategory);
+  }
 }
