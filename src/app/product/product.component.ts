675de875ed2928503ad7736034ba2b7776ae294c
ceoehis <ceo.ehis@outlook.com>
2018-03-29 18:59:19 +0100

    add product page functionality

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import { Location } from '@angular/common';
+
+import { Item } from '../item';
+import { Subcategory } from '../subCategory';
+import { ShoppingService } from '../shopping.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +14,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private route: ActivatedRoute, 
+    private shoppingService: ShoppingService,
+    private location: Location) { }
+  productName: string;
+  currentProduct: Item;
+  itemsList;
   ngOnInit() {
+    this.itemsList = this.shoppingService.getCategories();
+    this.route.queryParams
+      .filter(params => params.name)
+      .subscribe(params => {
+        this.productName = params.name;
+      });
+
+    this.currentProduct = this.getProduct(this.productName);
+    console.log(this.currentProduct);
+  }
+
+  getProduct(productName: string): Item {
+    var foundProduct: Item
+    this.itemsList.forEach(category => {
+      var subCategories: Subcategory[] = category.subcategories;
+      subCategories.forEach(subCategory => {
+        var products: Item[] = subCategory.items;
+        products.forEach(product => {
+          if(product.name == productName) {
+            foundProduct = product;
+            return;
+          }
+        })
+      })
+    })
+    return foundProduct;
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
