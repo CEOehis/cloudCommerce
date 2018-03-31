1a0f2abd6d17bdd40ced7339f3d8fe50de3a11c3
ceoehis <ceo.ehis@outlook.com>
2018-03-31 17:07:31 +0100

    add contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  contactForm: FormGroup;
+  name: FormControl;
+  email: FormControl;
+  subject: FormControl;
+  message: FormControl;
+
+  subjects: string[] = [
+    'inferior product',
+    'cancel purchase',
+    'positive review',
+    'negative review',
+    'other'
+  ]
   constructor() { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.name = new FormControl('', [Validators.required, Validators.minLength(4)]),
+    this.email = new FormControl('',[Validators.email, Validators.required]),
+    this.subject = new FormControl('', Validators.required),
+    this.message = new FormControl('',Validators.required)
+  }
+
+  createForm() {
+    this.contactForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      subject: this.subject,
+      message: this.message
+    })
   }
 
 }
